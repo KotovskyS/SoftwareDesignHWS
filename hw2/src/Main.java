6193c4bb35de74c2997f929282d6e2a469745d64
KotovskyS <sokotovskiy@edu.hse.ru>
2022-11-19 15:47:00 +0300

    Updated HW2

--- hw2/src/Main.java
+++ hw2/src/Main.java
@@ -12,44 +12,45 @@ public class Main {
         }
         System.out.println("/r - Выбор случайного студента \n/l - Вывод списка студентов \n/end - Завершить программу" );
         System.out.print(">");
-        String input = scanner.next();
+        String command = scanner.next();
         while (true) {
-            if (Objects.equals(input, "/r")){
-                int rndStudent = random.nextInt(studentList.size());
-                if (studentList.get(rndStudent).getMark() == 0) {
-                    System.out.println("Студент " + studentList.get(rndStudent).getSurname() + " отвечает:");
-                    System.out.println("Студент присутствует? да/нет");
-                    String answer = scanner.next();
-                    if (answer.equals("да")) {
-                        studentList.get(rndStudent).setAbsence(false);
-                        System.out.println("Введите оценку: ");
-                        int grade = scanner.nextInt();
-                        studentList.get(rndStudent).setMark(grade);
-                    } else if (answer.equals("нет")) {
-                        studentList.get(rndStudent).setAbsence(true);
-                        System.out.println("Студент " + studentList.get(rndStudent).getSurname() + " отсутствует!");
-                        studentList.get(rndStudent).setMark(0);
+            switch (command){
+                case "/r":
+                    int rndStudent = random.nextInt(studentList.size());
+                    if (studentList.get(rndStudent).getMark() == 0) {
+                        System.out.println("Студент " + studentList.get(rndStudent).getSurname() + " отвечает:");
+                        System.out.println("Студент присутствует? да/нет");
+                        String answer = scanner.next();
+                        if (answer.equals("да")) {
+                            studentList.get(rndStudent).setAbsence(false);
+                            System.out.println("Введите оценку: ");
+                            int grade = scanner.nextInt();
+                            studentList.get(rndStudent).setMark(grade);
+                        } else if (answer.equals("нет")) {
+                            studentList.get(rndStudent).setAbsence(true);
+                            System.out.println("Студент " + studentList.get(rndStudent).getSurname() + " отсутствует!");
+                            studentList.get(rndStudent).setMark(0);
+                        } else {
+                            System.out.println("Некорректный ввод!");
+                        }
                     } else {
-                        System.out.println("Некорректный ввод!");
+                        System.out.println("Студента " + studentList.get(rndStudent).getSurname() + " уже спрашивали!");
+                        System.out.println("Выбор случайного студена: /r.");
                     }
-                } else {
-                    System.out.println("Студента " + studentList.get(rndStudent).getSurname() + " уже спрашивали!");
-                    System.out.println("Выбор случайного студена: /r.");
-                }
-            }
-            else if (Objects.equals(input, "/l")){
-                for (Student student : studentList) {
-                    System.out.println(student);
-                }
-            }
-            else if (Objects.equals(input, "end")){
-                return;
-            }
-            else{
-                System.out.print("Некорректный ввод!\n");
+                    break;
+                case "/l":
+                    for (Student student : studentList) {
+                        System.out.println(student);
+                    }
+                    break;
+                case "end":
+                    return;
+                default:
+                    System.out.print("Некорректный ввод!\n");
+                    break;
             }
             System.out.print(">");
-            input = scanner.next();
+            command = scanner.next();
         }
     }
 }
